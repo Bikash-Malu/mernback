fea45a0b902b88f9ce44b7ce337f933db1dd9d47
BIKASH MALU <bikashmalu1@gmail.com>
2023-12-30 14:16:51 +0530

    add

--- Config.js
+++ Config.js
@@ -1,10 +1,13 @@
+const { ServerSelectionError } = require('mongodb');
+try {
+
+
 const mongoose=require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
-const { ServerSelectionError } = require('mongodb');
-// Example: Simulating a ServerSelectionError
-try {
   // Code that may result in a ServerSelectionError
   throw new ServerSelectionError('Unable to select a server for the operation.');
+
+
 } catch (error) {
   if (error instanceof ServerSelectionError) {
     console.error('Caught a ServerSelectionError:', error.message);
