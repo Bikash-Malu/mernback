6e8773ba1029bb875a872f279ff0d6bceda5727d
BIKASH MALU <bikashmalu1@gmail.com>
2023-12-30 14:07:19 +0530

    add

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2/ecommerce')
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
